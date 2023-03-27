31f3d1f746390d6f327e5e729bedbb96296b328c
GunHyun Jeong <gun.devcode@gmail.com>
2023-03-27 10:33:58 +0900

    Update 6_list

--- Beginner Course - notes/6_lists.py
+++ Beginner Course - notes/6_lists.py
@@ -17,3 +17,44 @@ print(friends)
 
 lucky_numbers = [4, 8, 15, 16, 23, 42]
 friends = ["Bernard", "Jan", "Sona", "Toby", "Tony"]
+# friends.extend(lucky_numbers)
+# print(friends) = ["Bernard", "Jan", "Sona", "Toby", "Tony", 4, 8, 15, 16, 23, 42]
+
+# adds element to the end of the list
+friends.append("Creed")
+print(friends)
+
+# insert into list using index
+friends.insert(1, "Alpha")
+print(friends)
+
+# remove
+friends.remove("Jan")
+print(friends)
+
+# remove all elements
+# friends.clear()
+print(friends)
+
+# pop - remove last item
+friends.pop()
+print(friends)
+
+# finding element is in list
+print(friends.index("Toby"))
+# friends.index("Karen") - ValueError: 'Karen' is not in list
+
+# count
+print(friends.count("Toby"))
+
+# sort
+friends.sort()
+print(friends)
+
+# reverse
+friends.reverse()
+print(friends)
+
+# copy
+friends2 = friends.copy()
+print(friends2)
