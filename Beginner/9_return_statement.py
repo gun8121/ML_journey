4504f03ce019e83330d637529704c2e0aafc41a3
GunHyun Jeong <gunh.jeong@gmail.com>
2023-03-29 13:02:40 +0900

    add: return statement

---
+++ Beginner/9_return_statement.py
@@ -0,0 +1,10 @@
+# getting information back from the function
+
+def cube(num):
+    return num*num*num
+
+# return breaks out of function
+# anything after return will not be processed
+
+result = cube(4)
+print(result)
