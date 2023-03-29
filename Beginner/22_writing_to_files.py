827c523424ff3702e17ade2170b84639b84c660b
GunHyun Jeong <gunh.jeong@gmail.com>
2023-03-29 16:59:09 +0900

    add: writing to files

---
+++ Beginner/22_writing_to_files.py
@@ -0,0 +1,8 @@
+
+employee_file = open('21_external_file.txt', "a")
+
+employee_file.write("\nKelly - Customer Service") # \n to add into new line
+
+employee_file.close()
+
+# when we use 'w' it will overwrite the whole file with kelly
