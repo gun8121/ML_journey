ce7dc2eaeadc8a19df444bc6adfbc21b86178697
GunHyun Jeong <gunh.jeong@gmail.com>
2023-03-29 14:24:21 +0900

    add: while loop

---
+++ Beginner/13_while_loop.py
@@ -0,0 +1,8 @@
+# while loop - execute repeatedly until a condition is met
+
+i = 1
+while i <= 10: # loop condition or loop guard - repeatedly executed while condition is true
+    print(i)
+    i = i+1 # or i += 1
+
+print("Done with loop")
