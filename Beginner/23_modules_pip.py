6f90c8976918221fc3be9771237a24224bab8ea5
GunHyun Jeong <gun.devcode@gmail.com>
2023-03-30 11:32:36 +0900

    add: modules & pip

---
+++ Beginner/23_modules_pip.py
@@ -0,0 +1,17 @@
+# module python file that can be imported into current python file
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+
+# more modules can be found on the official documentation
+# https://docs.python.org/3/py-modindex.html
+# community most likely has modules made for any task
+
+# built-in modules (built into python)
+# external modules - installed in same folder we installed python
+
+# what is pip? python3 pre-installed with pip
+# package manager
+# pip --version
+# pip install ####
+# pip uninstall ####
