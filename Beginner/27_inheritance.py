5ca59c936521c5e66a465dcd98b5fb6869a0f50b
GunHyun Jeong <gunh.jeong@gmail.com>
2023-04-01 11:57:22 +0900

    add: inheritance

---
+++ Beginner/27_inheritance.py
@@ -0,0 +1,11 @@
+# define attribute and function inside a class
+# make another class inherit that attributs or function
+
+from chef import Chef
+from chinesechef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
---
+++ Beginner/chinesechef.py
@@ -0,0 +1,10 @@
+from chef import Chef
+
+# inside chinesechef i want to use all the function in chef class
+class ChineseChef(Chef):
+
+    def make_special_dish(self):
+        print("The chef makes a orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
